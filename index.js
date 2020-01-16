8e64edc5c5c05c83832e593c4d5d6bfab8f124b4
Kevin Carvajal <kevin.carvajal.malmo@gmail.com>
2020-01-16 11:35:41 +0100

    Server/api/schema
    Startup for a server api

---
+++ index.js
@@ -0,0 +1,28 @@
+// Modules
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const theRest = require('the.rest');
+ 
+// Connect to MongoDB via Mongoose
+mongoose.connect('mongodb://localhost/db-name', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+ 
+// Create an Express server
+const app = express();
+ 
+// ..and install the.rest as middleware
+// Arguments/configuration:
+// 1) The express library
+// 2) The base route for the REST api to create
+// 3) The path to a folder with mongoose-models 
+//    Please Note: This path must be absolute
+const pathToModelFolder = path.join(__dirname, 'mongoose-models');
+app.use(theRest(express, '/api', pathToModelFolder));
+ 
+ 
+// Listen on port 5000
+app.listen(5000, () => console.log('Listening on port 5000'));
---
+++ schemas/User.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+let userSchema = new Schema({
+  personId: { type: String, unique: true, required: true },
+  username: { type: String, unique: true, },
+  role: { type: String, default: 'user' },
+  password: { type: String },
+  firstName: { type: String, required: true },
+  lastName: { type: String },
+  email: { type: String, unique: true, required: true },
+  phone: { type: String, unique: true },
+  active: { type: Boolean, default: false },
+})
+
+module.exports = mongoose.model("User", userSchema)
